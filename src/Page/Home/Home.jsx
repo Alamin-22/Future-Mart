91290dac3f34a081e1f72c830b214a3d74036e06
Alamin-22 <mdalaminmollick@gmail.com>
2024-02-03 13:30:55 +0600

    crated a basic navbar

--- src/Page/Home/Home.jsx
+++ src/Page/Home/Home.jsx
@@ -2,7 +2,9 @@
 const Home = () => {
     return (
         <div>
-            <h1 className="text-5xl text-red-800 flex min-h-screen justify-center items-center">This is home page</h1>
+            <h1 className="text-5xl text-red-800 flex min-h-screen justify-center items-center">
+                This is home page
+            </h1>
         </div>
     );
 };
--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,12 +1,21 @@
 import {
     createBrowserRouter,
 } from "react-router-dom";
+import Layout from "../Layout/Layout";
+import Error from "../Page/Error/Error";
 import Home from "../Page/Home/Home";
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <Home />,
+        element: <Layout></Layout>,
+        errorElement: <Error></Error>,
+        children: [
+            {
+                path: "/",
+                element: <Home></Home>
+            }
+        ]
     },
 ]);
 
