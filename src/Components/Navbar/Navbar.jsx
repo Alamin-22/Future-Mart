a983c8e9742d314fedcea728d9771a09d0a06e51
Alamin-22 <mdalaminmollick@gmail.com>
2024-02-03 13:55:31 +0600

    update navbar and make it condational based on user

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,12 @@
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
 
     const isAdmin = true;
+    const user = false;
     return (
         <div>
-            <div className="navbar bg-base-300">
+            <div className="navbar bg-gray-50 shadow-xl">
                 <div className="navbar max-w-7xl mx-auto">
                     <div className="flex-1">
                         <div className="btn btn-ghost text-lg">Future Mart</div>
@@ -28,25 +30,34 @@ const Navbar = () => {
                             </div>
                         </div>
                         <div>
-                            {}
+                            {
+                                user ?
+                                    <>
+                                        <div className="dropdown dropdown-end">
+                                            <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                                                <div className="w-10 rounded-full">
+                                                    <img alt="Tailwind CSS Navbar component" src="https://i.ibb.co/bFq9X83/user.png" />
+                                                </div>
+                                            </div>
+                                            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                                                <li>
+                                                    <a className="justify-between">
+                                                        Profile
+                                                        <span className="badge">New</span>
+                                                    </a>
+                                                </li>
+                                                <li><Link>Dashboard</Link></li>
+                                                <li><a>Logout</a></li>
+                                            </ul>
+                                        </div>
+                                    </>
+                                    :
+                                    <>
+                                        <div className="btn btn-outline btn-sm">Login</div>
+                                    </>
+                            }
                         </div>
-                        <div className="dropdown dropdown-end">
-                            <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-                                <div className="w-10 rounded-full">
-                                    <img alt="Tailwind CSS Navbar component" src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
-                                </div>
-                            </div>
-                            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                                <li>
-                                    <a className="justify-between">
-                                        Profile
-                                        <span className="badge">New</span>
-                                    </a>
-                                </li>
-                                <li><a>Settings</a></li>
-                                <li><a>Logout</a></li>
-                            </ul>
-                        </div>
+
                     </div>
                 </div>
             </div>
