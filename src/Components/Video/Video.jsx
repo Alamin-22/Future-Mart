767bdd997fa9f73cdc72609ba13bc79aab0ef823
Alamin-22 <mdalaminmollick@gmail.com>
2024-02-03 22:19:02 +0600

    first video and size layout is completed for all devices

--- src/Components/Video/Video.jsx
+++ src/Components/Video/Video.jsx
@@ -1,16 +1,26 @@
+import sizeChart from "../../assets/Images/size.webp"
+import { FaRegHandPointDown } from "react-icons/fa";
+
 
 const Video = () => {
     return (
-        <div>
-            <div className="relative overflow-hidden border">
+        <div className=" px-2 grid grid-cols-3 gap-5">
+            <div className="col-span-3 md:col-span-2 relative overflow-hidden ">
+                <div className=" flex items-center md:gap-2 mt-2">
+                    <p className="text-xl md:text-3xl"> মাত্র ৯৯৯ টাকায় ৪ টি টিশার্ট ভিন্ন ডিজাইনে</p>
+                    <FaRegHandPointDown className="text-2xl"></FaRegHandPointDown>
+                </div>
                 <iframe
-                    className="w-full md:w-3/4 h-[300px] md:h-[500px]"
+                    className="w-full h-[300px] md:h-[500px]"
                     src="https://www.youtube-nocookie.com/embed/gUdtW1nIdsE?rel=0&loop=1"
                     title="YouTube video player"
                     frameBorder="0"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                     allowFullScreen
                 ></iframe>
+            </div>
+            <div className=" col-span-3 md:col-span-1 ">
+                <img className="mx-auto h-[500px] md:mt-20 lg:mt-11" src={sizeChart} alt="T-shirt Size" />
             </div>
 
 
