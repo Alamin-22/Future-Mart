45a7703503b516d443237c12c5bfd6993d6ad64f
Alamin-22 <mdalaminmollick@gmail.com>
2024-02-03 12:58:38 +0600

    first commit

---
+++ src/Page/Home/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className="text-5xl text-red-800 flex min-h-screen justify-center items-center">This is home page</h1>
+        </div>
+    );
+};
+
+export default Home;
