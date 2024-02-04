bbb4604747dc9b6d4b38cf3c26a211a1a3d0966d
Alamin-22 <mdalaminmollick@gmail.com>
2024-02-04 21:10:04 +0600

    added radio for size in the t shirt

--- src/Components/CheckOut/CheckOut.jsx
+++ src/Components/CheckOut/CheckOut.jsx
@@ -10,26 +10,48 @@ const CheckOut = () => {
                             <h2 className="text-xl text-center font-medium">Billing Information </h2>
                             <div className="form-control">
                                 <label className="label">
-                                    <span className="label-text">আপনার নাম লিখুন  *</span>
+                                    <span className="label-text font-medium md:text-lg">আপনার নাম লিখুন <span className="text-red-600">*</span></span>
                                 </label>
                                 <input type="text" name="name" placeholder="আপনার নাম লিখুন " className="input input-bordered" required />
                             </div>
                             <div className="form-control">
                                 <label className="label">
-                                    <span className="label-text">আপনার ঠিকানা লিখুন  *</span>
+                                    <span className="label-text font-medium md:text-lg">আপনার ঠিকানা লিখুন  <span className="text-red-600">*</span></span>
                                 </label>
                                 <input type="text" name="name" placeholder="বাড়ি নং, রাস্তা নং, উপজেলা, জেলা" className="input input-bordered" required />
                             </div>
                             <div className="form-control">
                                 <label className="label">
-                                    <span className="label-text">আপনার মোবাইল নাম্বারটি লিখুন  *</span>
+                                    <span className="label-text font-medium md:text-lg">আপনার মোবাইল নাম্বারটি লিখুন  <span className="text-red-600">*</span></span>
                                 </label>
                                 <input type="number" name="name" placeholder="অনুগ্রহ করে আপনার বর্তমান ফোন নম্বর লিখুন যা সব সময় খোলা থাকবে" className="input input-bordered" required />
                             </div>
-                            
-                            <div className="form-control mt-6">
-                                <button className="btn btn-primary">Login</button>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text font-medium md:text-lg">আপনার সাইজ সিলেক্ট করুন  <span className="text-red-600">*</span></span>
+                                </label>
+                                <div className="space-y-2 space-x-3">
+                                    <label className="inline-flex cursor-pointer items-center">
+                                        <input type="radio" className="form-radio text-indigo-600" name="size" value="M" />
+                                        <span className="ml-1">M</span>
+                                    </label>
+
+                                    <label className="inline-flex items-center cursor-pointer">
+                                        <input type="radio" className="form-radio text-indigo-600" name="size" value="L" />
+                                        <span className="ml-1">L</span>
+                                    </label>
+
+                                    <label className="inline-flex items-center cursor-pointer">
+                                        <input type="radio" className="form-radio text-indigo-600" name="size" value="XL" />
+                                        <span className="ml-1">XL</span>
+                                    </label>
+                                    <label className="inline-flex items-center cursor-pointer">
+                                        <input type="radio" className="form-radio text-indigo-600" name="size" value="XXL" />
+                                        <span className="ml-1">XXL</span>
+                                    </label>
+                                </div>
                             </div>
+
                         </form>
                     </div>
                 </div>
