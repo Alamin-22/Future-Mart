a2d160fb9b5d08bf2a3711a625893edfc5433f0e
Alamin-22 <mdalaminmollick@gmail.com>
2024-02-04 20:43:09 +0600

    crated checkout layout

---
+++ src/Components/CheckOut/CheckOut.jsx
@@ -0,0 +1,18 @@
+
+const CheckOut = () => {
+    return (
+        <div className="h-screen border">
+            <div className="md:flex  bg-red-50 h-screen">
+                <div className=" w-full bg-green-200">
+                    asd
+                </div>
+                <div className=" w-full  bg-red-200">
+
+                    asldkalsdk;
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default CheckOut;
--- src/Page/Home/Home.jsx
+++ src/Page/Home/Home.jsx
@@ -1,3 +1,4 @@
+import CheckOut from "../../Components/CheckOut/CheckOut";
 import Gallery from "../../Components/Gallery/Gallery";
 import Video from "../../Components/Video/Video";
 
@@ -7,6 +8,8 @@ const Home = () => {
             <Video></Video>
             <h2 className="text-center text-3xl md:text-5xl font-semibold  my-5"> আমাদের পণ্য </h2>
             <Gallery></Gallery>
+            <h2 className="text-center text-3xl text-red-600 md:text-5xl font-semibold  my-5"> অর্ডার করতে সঠিক তথ্য দিয়ে নিচের ফর্ম পূরন করুন</h2>
+            <CheckOut></CheckOut>
         </div>
     );
 };
