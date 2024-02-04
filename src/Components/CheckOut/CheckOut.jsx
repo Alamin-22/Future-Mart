2b8e73ba2901f5a0e5a2fa4deb52fa349bc73d55
Alamin-22 <mdalaminmollick@gmail.com>
2024-02-04 20:55:40 +0600

    crated form for chekout

--- src/Components/CheckOut/CheckOut.jsx
+++ src/Components/CheckOut/CheckOut.jsx
@@ -4,7 +4,34 @@ const CheckOut = () => {
         <div className="h-screen border">
             <div className="md:flex  bg-red-50 h-screen">
                 <div className=" w-full bg-green-200">
-                    asd
+                    <div className="card shrink-0 w-full  shadow-2xl bg-base-100">
+                        <form className="card-body">
+
+                            <h2 className="text-xl text-center font-medium">Billing Information </h2>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">আপনার নাম লিখুন  *</span>
+                                </label>
+                                <input type="text" name="name" placeholder="আপনার নাম লিখুন " className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">আপনার ঠিকানা লিখুন  *</span>
+                                </label>
+                                <input type="text" name="name" placeholder="বাড়ি নং, রাস্তা নং, উপজেলা, জেলা" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">আপনার মোবাইল নাম্বারটি লিখুন  *</span>
+                                </label>
+                                <input type="number" name="name" placeholder="অনুগ্রহ করে আপনার বর্তমান ফোন নম্বর লিখুন যা সব সময় খোলা থাকবে" className="input input-bordered" required />
+                            </div>
+                            
+                            <div className="form-control mt-6">
+                                <button className="btn btn-primary">Login</button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
                 <div className=" w-full  bg-red-200">
 
--- src/Page/Home/Home.jsx
+++ src/Page/Home/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
             <Video></Video>
             <h2 className="text-center text-3xl md:text-5xl font-semibold  my-5"> আমাদের পণ্য </h2>
             <Gallery></Gallery>
-            <h2 className="text-center text-3xl text-red-600 md:text-5xl font-semibold  my-5"> অর্ডার করতে সঠিক তথ্য দিয়ে নিচের ফর্ম পূরন করুন</h2>
+            <h2 className="text-center text-3xl text-red-600 md:text-4xl font-semibold  my-5"> অর্ডার করতে সঠিক তথ্য দিয়ে নিচের ফর্ম পূরন করুন</h2>
             <CheckOut></CheckOut>
         </div>
     );
