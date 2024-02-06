418ece0d55cae70bb782da8a2fad189a9dca8daf
Alamin-22 <mdalaminmollick@gmail.com>
2024-02-06 12:26:32 +0600

    update sliding text

--- src/Components/SlidingText/SlidingText.jsx
+++ src/Components/SlidingText/SlidingText.jsx
@@ -1,13 +1,12 @@
 import Marquee from "react-fast-marquee";
 
 
-
 const SlidingText = () => {
     return (
         <div>
             <div className="mx-2 p-2 bg-red-500 text-white text-xl my-1 rounded-xl   ">
-                <Marquee className=" ">
-                    যেকোন বা প্রশ্নের জন্য এই নম্বরে যোগাযোগ করুন +880 1604-134742, +880 1317-543508
+                <Marquee className="">
+                আমাদের দোকান সাত মসজিদ সুপার মার্কেট এর ২য় তলা মোহাম্মদপুর,ঢাকা । যেকোন বা প্রশ্নের জন্য এই নম্বরে যোগাযোগ করুন +880 1604-134742, +880 1317-543508 ।  
                 </Marquee>
             </div>
         </div>
